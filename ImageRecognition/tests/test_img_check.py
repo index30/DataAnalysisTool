a226de11d28ed19dc9491c5225487954f2abd9cf
ranthy <realthingshakes25@gmail.com>
2019-12-29 00:19:43 +0900

    Modified test code for img_check, img_utils.

--- ImageRecognition/tests/test_img_check.py
+++ ImageRecognition/tests/test_img_check.py
@@ -8,21 +8,26 @@ import img_check
 class TestImgCheck(unittest.TestCase):
     def setUp(self):
         self.ic = img_check.ImgCheck("sample_data")
+        self.img_suffix = ['jpg', 'JPG', 'png']
     
     def tearDown(self):
         del self.ic
         
     def test_mk_img_tree(self):
         tree_result = self.ic.mk_img_tree()
-        print(tree_result)
         self.assertEqual(len(tree_result), 2)
         
     def test_rec_random_pick_imgs(self):
         imgs_dict = self.ic.rec_random_pick_imgs()
-        self.assertEqual(len(imgs_dict), 2)
-        self.assertCountEqual(list(imgs_dict.keys()), self.ic.mk_img_tree())
+        # Check this method can pick up from directory which has image
+        self.assertEqual(len(imgs_dict), len(self.ic.mk_img_tree()))
+        for img_key in imgs_dict.keys():
+            # Check this method can pick without not image suffix 
+            self.assertIn(Path(img_key).suffix[1:], self.img_suffix)
         
-        
+    def test_rec_random_pick_annotations(self):
+        # TODO: Set annotations
+        pass
 
 
 if __name__ == "__main__":
--- ImageRecognition/tests/test_img_utils.py
+++ ImageRecognition/tests/test_img_utils.py
@@ -7,7 +7,7 @@ import img_utils
 
 class TestImgUtils(unittest.TestCase):
     def setUp(self):
-        pass
+        self.category_list = ["food", "other"]
     
     def tearDown(self):
         pass
@@ -17,7 +17,15 @@ class TestImgUtils(unittest.TestCase):
         pasta_list = img_utils.mk_img_list("sample_data/pasta")
         self.assertEqual(len(other_list), 8)
         self.assertEqual(len(pasta_list), 8)
-
+    
+    def test_json_data(self):
+        # TODO: Set Json Data
+        pass
+    
+    def test_select_univ_clr(self):
+        category_clr_list = img_utils.select_univ_clr(self.category_list)
+        self.assertEqual(len(list(category_clr_list.keys())), 
+                         len(self.category_list))
 
 if __name__ == "__main__":
     unittest.main()
