13094f442673654ad140d4285610bc935cd19cfe
ranthy <realthingshakes25@gmail.com>
2020-01-13 22:24:59 +0900

    Add function for xml in annotation

--- ImageRecognition/img_utils.py
+++ ImageRecognition/img_utils.py
@@ -1,6 +1,7 @@
 import itertools
 import json
 from pathlib import Path
+import xml.etree.ElementTree as ET
 
 # 赤, 黄, 緑, ピンク, 青, 茶, オレンジ, 空色, 明るい紫, クリーム
 # グレー, 明るい緑
@@ -25,21 +26,42 @@ def mk_img_list(imgs_root):
 
 def fetch_json_data(json_path):
     '''Fetch the data of json
-    # Arguments
+    # Argument
     json_path:      input path for fetching json path
+    
+    # Return
+    data:           json data
     '''
     with open(Path(json_path).as_posix(), 'r') as json_file:
         data = json.load(json_file)
     return data
 
-def select_univ_clr(target_category_list):
+def fetch_xml_data(xml_path):
+    '''Fetch the data of xml
+    # Argument
+    xml_path:       input path for fetching xml path
+    
+    # Return
+    xml_tree:       xml data
+    '''
+    xml_tree = ET.parse(Path(xml_path).as_posix())
+    return xml_tree
+
+def select_univ_clr(src_target_category):
     '''Return color list correspond to UNIV_CLR_LIST
     # Argument
-    target_category:        the list of category
+    src_target_category:    the source of category
+                            its type is Path(includes ***.txt etc.) or list
     # Return
     category_clr_dict       the dict of color and category
     '''
     category_clr_dict = {}
+    if type(src_target_category) != list:
+        with open(Path(src_target_category).as_posix(), 'r') as src_file:
+            target_category_list = [readline[:-1] for readline in src_file.readlines()]
+    else:
+        target_category_list = src_target_category
+        
     for tc, ucl in zip(target_category_list, UNIV_CLR_LIST):
         category_clr_dict[tc] = ucl    
     return category_clr_dict
