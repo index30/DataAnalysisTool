789ad6e07eed78a39acfe0e0d825bcfe7fdf4da9
ranthy <realthingshakes25@gmail.com>
2020-01-13 22:26:16 +0900

    Add testcode for fetch_xml_data

--- ImageRecognition/tests/test_img_utils.py
+++ ImageRecognition/tests/test_img_utils.py
@@ -7,7 +7,8 @@ import img_utils
 
 class TestImgUtils(unittest.TestCase):
     def setUp(self):
-        self.category_list = ["food", "other"]
+        self.category_list = ["drink", "other", "pasta"]
+        self.class_file_path = Path(Path.cwd(), "sample_data/classes.txt")
     
     def tearDown(self):
         pass
@@ -18,14 +19,20 @@ class TestImgUtils(unittest.TestCase):
         self.assertEqual(len(other_list), 8)
         self.assertEqual(len(pasta_list), 8)
     
-    def test_json_data(self):
+    def test_fetch_json_data(self):
         # TODO: Set Json Data
+        pass
+    
+    def test_fetch_xml_data(self):
+        # TODO: Set XML Data
         pass
     
     def test_select_univ_clr(self):
         category_clr_list = img_utils.select_univ_clr(self.category_list)
         self.assertEqual(len(list(category_clr_list.keys())), 
                          len(self.category_list))
+        self.assertListEqual(self.category_list,
+                             list(category_clr_list.keys()))
 
 if __name__ == "__main__":
     unittest.main()
