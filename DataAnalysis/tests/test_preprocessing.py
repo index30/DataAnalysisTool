04fd5058df8e2f21dfacee5a983f7282bf5d88cd
ranthy <realthingshakes25@gmail.com>
2020-01-28 00:46:47 +0900

    Modified gitignore for pyvenv.cfg, Add test mock code in test_preprocessing.py

--- DataAnalysis/tests/test_preprocessing.py
+++ DataAnalysis/tests/test_preprocessing.py
@@ -14,6 +14,27 @@ class TestPreprocessing(unittest.TestCase):
 
     def test_load_data(self):
         pass
+    
+    def test_load_feature_list(self):
+        pass
+    
+    def cut_na(self):
+        pass
+    
+    def split_Xy(self):
+        pass
+    
+    def test_pickup_corr_lists(self):
+        pass
+    
+    def test_devide_intfloat(self):
+        pass
+    
+    def test_devide_binary(self):
+        pass
+    
+    def test_val2int(self):
+        pass
 
 if __name__ == "__main__":
     unittest.main()
